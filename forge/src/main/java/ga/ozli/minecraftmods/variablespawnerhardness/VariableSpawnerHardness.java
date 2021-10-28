3f7d66a1d553dd492113467e51506c47dcd96c14
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2021-10-28 03:01:25 +0200

    port to architectury

---
+++ forge/src/main/java/ga/ozli/minecraftmods/variablespawnerhardness/VariableSpawnerHardness.java
@@ -0,0 +1,8 @@
+package ga.ozli.minecraftmods.variablespawnerhardness;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("variablespawnerhardness")
+public final class VariableSpawnerHardness {
+
+}
