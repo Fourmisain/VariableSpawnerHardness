467ea16c813e34974076665cc8857358a5256c67
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2021-10-28 04:46:05 +0200

    switch to mojang mappings for some reason the refmaps wouldn't work for forge 1.16 even though they work in 1.17 and fabric had no issues at all

--- common/src/main/java/ga/ozli/minecraftmods/variablespawnerhardness/mixin/AbstractBlockStateMixin.java
+++ common/src/main/java/ga/ozli/minecraftmods/variablespawnerhardness/mixin/AbstractBlockStateMixin.java
@@ -1,21 +1,20 @@
 package ga.ozli.minecraftmods.variablespawnerhardness.mixin;
 
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.BlockView;
-import net.minecraft.world.WorldAccess;
-import net.minecraft.world.WorldProperties;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.storage.LevelData;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(AbstractBlock.AbstractBlockState.class)
+@Mixin(BlockBehaviour.BlockStateBase.class)
 public abstract class AbstractBlockStateMixin {
-
 	@Shadow
 	public abstract Block getBlock();
 
@@ -23,16 +22,16 @@ public abstract class AbstractBlockStateMixin {
 	 * @reason Make block hardness variable for spawner blocks depending on world difficulty
 	 * @author Paint_Ninja
 	 */
-	@Inject(method = "getHardness(Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;)F", at = @At("TAIL"), cancellable = true)
-	public void getHardness(BlockView worldIn, BlockPos pos, CallbackInfoReturnable<Float> cir) {
-		if (this.getBlock() == Blocks.SPAWNER && worldIn instanceof WorldAccess) {
-			cir.setReturnValue(onGetBlockHardness(worldIn));
+	@Inject(method = "getDestroySpeed(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)F", at = @At("TAIL"), cancellable = true)
+	public void getDestroySpeed(BlockGetter blockGetter, BlockPos blockPos, CallbackInfoReturnable<Float> cir) {
+		if (this.getBlock() == Blocks.SPAWNER && blockGetter instanceof Level) {
+			cir.setReturnValue(onGetDestroySpeed(blockGetter));
 		}
 	}
 
 	private static final float[] hardnessByDifficulty = {5F, 9F, 22.5F, 30F};
-	private static float onGetBlockHardness(BlockView worldIn) {
-		final WorldProperties worldInfo = ((WorldAccess) worldIn).getLevelProperties();
-		return worldInfo.isHardcore() ? 50F : hardnessByDifficulty[worldInfo.getDifficulty().ordinal()];
+	private static float onGetDestroySpeed(BlockGetter blockGetter) {
+		LevelData levelData = ((Level) blockGetter).getLevelData();
+		return levelData.isHardcore() ? 50F : hardnessByDifficulty[levelData.getDifficulty().ordinal()];
 	}
 }
